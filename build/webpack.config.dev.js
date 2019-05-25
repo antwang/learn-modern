35f4445b6ab06059e0842c9a220d3ca17c46b19e
wangchao <wangchao@daojia.com>
2019-05-25 16:27:34 +0800

    delete unused file

--- build/webpack.config.dev.js
+++
@@ -1,7 +0,0 @@
-const merge = require("webpack-merge");
-const baseConf = require("./webpack.config.base");
-
-module.exports = merge(baseConf, {
-  mode: "development",
-  devtool: "eval-source-map"
-});
